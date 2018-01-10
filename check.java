adc23dedb6ff581c79db814e8170a212034a6dcd
Rafaila98 <32717035+Rafaila98@users.noreply.github.com>
2018-01-10 12:19:34 +0200

    Add files via upload

---
+++ check.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class check {
+ public static void checker(String[] strs,HashSet<String> dicitonary)
+ int i=0;
+ while (strs[i].hasNext=true) {
+	 if (dictionary.contains(strs[i]))=false {
+		 System.out.println(strs[i]);
+	 }
+ }
+}
